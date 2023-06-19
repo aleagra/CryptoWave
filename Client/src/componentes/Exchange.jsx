87ca1210f60eddae7bc6ebe7a5d759c461bd3460
Alejandro Agra <aleagra08@hotmail.com>
2023-06-19 16:21:38 -0300

    styles

--- Client/src/componentes/Exchange.jsx
+++ Client/src/componentes/Exchange.jsx
@@ -90,12 +90,81 @@ function Exchange() {
   }
   return (
     <>
-      <div className="grid grid-cols-[300px,1fr,300px] grid-rows-[100px,1fr] gap-12 bg-[#121418]">
+      <div className="grid grid-cols-[300px,1fr,300px] grid-rows-[100px,1fr] gap-12 ">
         <Navbar />
         <div className="col-start-1 row-start-2 h-[700px] w-full rounded-lg bg-[#121418] py-20 max-2xl:h-[450px] max-2xl:py-6"></div>
-        <div className="col-start-2 row-start-2 h-[700px] w-full rounded-lg bg-[#121418] py-20 max-2xl:h-[450px] max-2xl:py-6"></div>
-        <section className="col-start-3 row-start-2 flex h-[700px] justify-center rounded-lg  py-20 text-white max-2xl:h-[450px] max-2xl:py-6">
-          <div className=" m-auto flex w-full  flex-col gap-y-6 p-5">
+        <div className="col-start-2 row-start-2 h-fit w-full rounded-lg bg-[#121418] pt-6 max-2xl:h-[450px] max-2xl:py-6">
+          <div className="m-auto w-[100%] text-left text-sm text-white">
+            <div className="grid grid-cols-5 pl-6 text-lg font-semibold text-[#7D7D7D]">
+              <div className="text-center max-sm:hidden">Type</div>
+              <div className="text-center max-md:pl-4">Coin</div>
+              <div className="text-center">Amount</div>
+              <div className="text-center max-lg:hidden">Total Value</div>
+              <div className="text-center max-lg:hidden">Status</div>
+            </div>
+
+            <div className="my-5 border-opacity-30">
+              {coins?.slice(0, 7).map((Element) => {
+                const numericValue = parseFloat(
+                  Element.DISPLAY?.USD.CHANGE24HOUR.substring(1)
+                );
+                return (
+                  <div
+                    className="grid grid-cols-5 place-items-center rounded-md py-5 pl-6 hover:bg-white/10"
+                    key={Element.CoinInfo.Id}
+                  >
+                    <div className=" flex items-center gap-2 text-lg max-lg:hidden">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="h-7 w-7 rounded-full bg-yellow-400 stroke-black p-[5px]"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M7.5 21L3 16.5m0 0L7.5 12M3 16.5h13.5m0-13.5L21 7.5m0 0L16.5 12M21 7.5H7.5"
+                        />
+                      </svg>
+
+                      <span>Exchange</span>
+                    </div>
+                    <div className="flex w-full justify-center  gap-2">
+                      <img
+                        className="h-6 w-6"
+                        src={
+                          "https://www.cryptocompare.com" +
+                          Element.CoinInfo.ImageUrl
+                        }
+                        alt={Element.CoinInfo.FullName}
+                      />
+                      <p className="text-xl">
+                        {<p className="text-xl">{Element.CoinInfo.Internal}</p>}
+                      </p>
+                    </div>
+
+                    <div className="text-center text-xl font-bold">
+                      0,002342
+                    </div>
+
+                    <div className="flex items-center justify-center  max-lg:hidden">
+                      <p>$423,40</p>
+                    </div>
+                    <div className="flex items-center justify-center gap-2  max-lg:hidden">
+                      <p className="rounded-lg bg-yellow-400/20 p-2 text-yellow-400 ">
+                        In progress
+                      </p>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+        <section className="col-start-3 row-start-2 flex h-fit justify-center  rounded-lg bg-[#121418] text-white max-2xl:h-[450px] max-2xl:py-6">
+          <div className="flex w-full flex-col gap-y-6 p-10">
             <h1 className="text-center text-xl">Traiding spot</h1>
             <div className="relative">
               <Select
@@ -148,7 +217,7 @@ function Exchange() {
             </div>
             <div className=" flex justify-center">
               <button
-                className="w-full rounded-md bg-[#00A68C] py-3 font-bold uppercase"
+                className="w-full rounded-md bg-[#46429d] py-3 font-bold uppercase"
                 onClick={() => {
                   parseInt(myData?.balance) >= amount
                     ? handleSubmit() + alerta()
