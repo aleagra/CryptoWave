08c1bb1a5d3165aab599dbd68e9f8537ab01c9f0
Alejandro Agra <aleagra08@hotmail.com>
2023-06-22 16:14:01 -0300

    host firebase

---
+++ Client/src/firebase.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAA-PN2V7zLpRYVdn1eHI0MF2Bu9FprWvk",
+  authDomain: "cryptowave-d3f50.firebaseapp.com",
+  projectId: "cryptowave-d3f50",
+  storageBucket: "cryptowave-d3f50.appspot.com",
+  messagingSenderId: "650190189495",
+  appId: "1:650190189495:web:fa8f71d957232e0ffb6e32"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
