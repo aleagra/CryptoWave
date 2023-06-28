63ae16610c5f5624c2d9049ac41858281efa2536
Alejandro Agra <aleagra08@hotmail.com>
2023-06-28 15:41:28 -0300

    delete backend

--- Api rest/config/utils.bs.js
+++
@@ -1,7 +0,0 @@
-const { User } = require("../model/user.model");
-const { Coin } = require("../model/coin.model");
-const databaseMysql = require("./mysql.config");
-
-User.sync();
-Coin.sync();
-databaseMysql.sync();
